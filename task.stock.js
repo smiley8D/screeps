2d81b9be14204a88847069a306acaa8b268db873
smiley <smiley@smiley.bz>
2025-02-16 00:00:00 +0000

    Full Stock implementation

--- task.stock.js
+++ task.stock.js
@@ -13,24 +13,42 @@ class Stock extends Task {
         // Get totals
         let amount = 0;
         let capacity = 0;
-        for (let structure of room.find(FIND_STRUCTURES, { filter: (o) => (o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) })) {
+        for (let structure of room.find(FIND_STRUCTURES, { filter: (o) =>
+            (o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) &&
+            o.pos.lookFor(LOOK_FLAGS).length == 0 })) {
             amount += structure.store.getUsedCapacity(RESOURCE_ENERGY);
             capacity += structure.store.getCapacity(RESOURCE_ENERGY);
         }
         let avg_fill = amount / capacity;
 
-        // Get imbalance
-        let imbalance = 0;
-        for (let structure of room.find(FIND_STRUCTURES, { filter: (o) => (o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) })) {
-            let diff = (structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY)) - avg_fill;
-            if (diff > 0.1) {
-                imbalance += structure.store.getCapacity(RESOURCE_ENERGY) * diff;
+        // Get imbalances
+        let over = 0;
+        let under = 0;
+        for (let structure of room.find(FIND_STRUCTURES, { filter: (o) => o.store && (o.my || !o.owner) } )) {
+            if (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) {
+                if (structure.pos.lookFor(LOOK_FLAGS, {filter: {color: COLOR_YELLOW}}).length > 0) {
+                    // Flagged as empty
+                    over += structure.store.getUsedCapacity(RESOURCE_ENERGY);
+                } else if (structure.pos.lookFor(LOOK_FLAGS, {filter: {color: COLOR_BLUE}}).length > 0) {
+                    // Flagged as fill
+                    under += structure.store.getFreeCapacity(RESOURCE_ENERGY);
+                } else {
+                    // Non-flagged container/storage, check against avg
+                    let diff = structure.store.getCapacity(RESOURCE_ENERGY) * ((structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY)) - avg_fill);
+                    if (diff > 0) { over += diff }
+                    else if (diff < 0) { under -= diff }
+                }
+            } else {
+                // Not a container or storage, always fill
+                under += structure.store.getFreeCapacity(RESOURCE_ENERGY);
             }
         }
 
         // Create task
-        if (imbalance > 0) {
-            let task = new Stock(room.name, Math.max(0, Math.round(Math.log(imbalance) / Math.log(25))), avg_fill);
+        let imbalance = Math.max(over, under);
+        let workers = Math.max(0, Math.round(Math.log(Math.abs(imbalance)) / Math.log(50)));
+        if (workers > 0) {
+            let task = new Stock(room.name, workers, avg_fill);
             return [task];
         }
         return [];
@@ -46,13 +64,24 @@ class Stock extends Task {
             return;
         }
 
-        // Find most filled container
+        // Find most full valid src
         if ((!creep.memory.curFill && !creep.store.getUsedCapacity(RESOURCE_ENERGY)) || (!creep.memory.curFill && !creep.memory.curDepo)) {
             let cur = 0;
-            for (let structure of room.find(FIND_STRUCTURES, { filter: (o) => (o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) })) {
-                if (structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY) > cur) {
-                    cur = structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY);
-                    creep.memory.curFill = structure.id;
+            let cur_flagged = 0;
+            for (let structure of room.find(FIND_MY_STRUCTURES, { filter: (o) => o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE } )) {
+                let filled = structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY);
+                if (structure.pos.lookFor(LOOK_FLAGS, {filter: {color: COLOR_YELLOW}}).length > 0) {
+                    // Flagged as empty
+                    if (filled > cur_flagged) {
+                        cur_flagged = filled;
+                        creep.memory.curFill = structure.id;
+                    }
+                } else if (structure.pos.lookFor(LOOK_FLAGS).length == 0) {
+                    // Unflagged
+                    if (filled > cur && !cur_flagged) {
+                        cur = filled;
+                        creep.memory.curFill = structure.id;
+                    }
                 }
             }
         }
@@ -70,17 +99,34 @@ class Stock extends Task {
             } else if (result != OK) {
                 // Cannot complete task
                 creep.memory.task = null;
-                creep.say(result);
             }
         }
 
-        // Find least filled container
+        // Find most imbalanaced dst
         if (!creep.memory.curDepo && !creep.store.getFreeCapacity(RESOURCE_ENERGY)) {
-            let cur = 1;
-            for (let structure of room.find(FIND_STRUCTURES, { filter: (o) => (o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) })) {
-                if (structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY) < cur) {
-                    cur = structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY);
-                    creep.memory.curDepo = structure.id;
+            let cur = 0;
+            let cur_flagged = 0;
+            let cur_other = 0;
+            for (let structure of room.find(FIND_MY_STRUCTURES, { filter: (o) => o.store && (o.my || !o.owner) } )) {
+                let empty = structure.store.getFreeCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY);
+                if (structure.structureType != STRUCTURE_CONTAINER || structure.structureType != STRUCUTRE_STORAGE) {
+                    // Not container or storage
+                    if (empty > cur_other) {
+                        cur_other = empty;
+                        creep.memory.curDepo = structure.id;
+                    }
+                } else if (structure.pos.lookFor(LOOK_FLAGS, {filter: {color: COLOR_BLUE}}).length > 0) {
+                    // Flagged as fill
+                    if (empty > cur_flagged & !cur_other) {
+                        cur_flagged = empty;
+                        creep.memory.curDepo = structure.id;
+                    }
+                } else if (structure.pos.lookFor(LOOK_FLAGS).length == 0) {
+                    // Unflagged
+                    if (empty > cur && !cur_flagged && !cur_other) {
+                        cur = empty;
+                        creep.memory.curDepo = structure.id;
+                    }
                 }
             }
         }
@@ -98,7 +144,6 @@ class Stock extends Task {
             } else if (result != OK) {
                 // Cannot complete task
                 creep.memory.task = null;
-                creep.say(result);
             }
         }
     }
