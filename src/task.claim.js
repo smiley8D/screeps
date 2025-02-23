cef4e4cbf114c446f62fa3a72202866af639894c
smiley <smiley@smiley.bz>
2025-02-23 00:00:00 +0000

    Update claim task with flag changes

--- src/task.claim.js
+++ src/task.claim.js
@@ -6,9 +6,11 @@ class Claim extends Task {
 
     static emoji = '🚩';
 
-    constructor(room, wanted) {
+    constructor(room, action, wanted) {
         super("Claim", room, room, wanted);
         this.body = new Claimer();
+        this.action = action;
+        this.detail = action;
     }
 
     static getTasks() {
@@ -17,11 +19,41 @@ class Claim extends Task {
         // Iterate through claim flags
         for (let flag in Game.flags) {
             flag = Game.flags[flag];
-            if (flag.color != COLOR_PURPLE) {continue}
-            if (!flag.room || !flag.room.controller.my) {
-                tasks.push(new Claim(flag.pos.roomName, 1));
-            } else {
+
+            // Check flag
+            if (flag.color != COLOR_YELLOW) { continue }
+            else if (flag.room && ((flag.color != COLOR_BROWN && !flag.room.controller) ||
+            (flag.room.controller && flag.room.controller.my))) {
                 flag.remove();
+                continue;
+            }
+
+            // Handle actions
+            switch (flag.secondaryColor) {
+                case COLOR_YELLOW:
+                    // Claim
+                    if (flag.room && flag.room.controller.owner) {
+                        flag.remove();
+                        continue;
+                    }
+                    tasks.push(new Claim(flag.pos.roomName, 'claim', 1));
+                    break;
+                case COLOR_BLUE:
+                    // Reserve
+                    if (flag.room && flag.room.controller.owner) {
+                        flag.remove();
+                        continue;
+                    }
+                    tasks.push(new Claim(flag.pos.roomName, 'reserve', 2));
+                    break;
+                case COLOR_RED:
+                    // Attack
+                    if (flag.room && !flag.room.controller.owner) {
+                        flag.remove();
+                        continue;
+                    }
+                    tasks.push(new Claim(flag.pos.roomName, 'attack', 1));
+                    break;
             }
         }
 
@@ -34,10 +66,26 @@ class Claim extends Task {
             return creep.memory.task.room;
         }
 
-        // Claim controller
         let controller = Game.rooms[creep.memory.task.room].controller;
-        let result = creep.claimController(controller);
-        if (result === ERR_GCL_NOT_ENOUGH) { result = creep.reserveController(controller) }
+        let result = ERR_NOT_FOUND;
+
+        // Handle actions
+        switch (creep.memory.task.detail) {
+            case 'claim':
+                // Claim
+                result = creep.claimController(controller);
+                break;
+            case 'reserve':
+                // Reserve
+                result = creep.reserveController(controller);
+                break;
+            case 'attack':
+                // Attack
+                result = creep.attackController(controller);
+                break;
+        }
+
+        // Move if needed
         if (result === ERR_NOT_IN_RANGE) { result = creep.moveTo(controller, {visualizePathStyle: {stroke: "#991eff"}}) }
 
         return result;
